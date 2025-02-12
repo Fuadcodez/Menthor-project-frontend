4fe95245d22cdbf8af3adefb19f27faba81fab01
FuadCodez <fuadabolaji1@gmail.com>
2025-02-12 23:01:15 +0100

    g

--- rest-countries-main/src/components/ui/provider.tsx
+++ rest-countries-main/src/components/ui/provider.tsx
@@ -3,10 +3,11 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import {
   ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
 
-export function Provider(props: ColorModeProviderProps) {
+} from "./color-mode"
+import type { ThemeProviderProps } from "next-themes";
+
+export function Provider(props: ThemeProviderProps) {
   return (
     <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
